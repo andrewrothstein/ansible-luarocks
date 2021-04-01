e71e1dcf52afac8fc1fb8c4a3fc671ed7fc098dd
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-03-31 21:46:25 -0400

    luarocks:3.5.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env sh
-VER=${1:-3.4.0}
+#!/usr/bin/env bash
 DIR=~/Downloads
-FILE=luarocks-${VER}.tar.gz
-URL=http://luarocks.org/releases/$FILE
-LFILE=$DIR/$FILE
 
-if [ ! -e $LFILE ];
-then
-    wget -q -O $LFILE $URL
-fi
+dl() {
+    local -r ver=$1
+    local -r file=luarocks-${ver}.tar.gz
+    local -r url=http://luarocks.org/releases/$file
+    local -r lfile=$DIR/$file
 
-printf "  # %s\n" $URL
-printf "  '%s': sha256:%s\n" $VER $(sha256sum $LFILE | awk '{print $1}')
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl ${1:-3.5.0}
